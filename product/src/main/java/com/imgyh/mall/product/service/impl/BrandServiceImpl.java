a15dbcca66da0fa3526713389933768977714803
imgyh <1974355683@qq.com>
2023-02-27 16:24:12 +0800

    feat(brand): 品牌添加模糊查询

--- product/src/main/java/com/imgyh/mall/product/service/impl/BrandServiceImpl.java
+++ product/src/main/java/com/imgyh/mall/product/service/impl/BrandServiceImpl.java
@@ -1,16 +1,16 @@
 package com.imgyh.mall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.imgyh.mall.common.utils.PageUtils;
 import com.imgyh.mall.common.utils.Query;
-
 import com.imgyh.mall.product.dao.BrandDao;
 import com.imgyh.mall.product.entity.BrandEntity;
 import com.imgyh.mall.product.service.BrandService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("brandService")
@@ -18,9 +18,18 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        // 增加根据 key 模糊查询
+        String key = (String)params.get("key");
+        QueryWrapper<BrandEntity> wrapper = new QueryWrapper<BrandEntity>();
+        // select * from pms_brand where brand_id=key or name like key
+
+        if (key != null && !key.isEmpty()){
+            wrapper.eq("brand_id", key).or().like("name",key);
+        }
+
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
